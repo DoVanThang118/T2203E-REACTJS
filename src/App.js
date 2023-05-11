2816d7a7d432926428228e5e83f77f0e69e29cff
DoVanThang118 <102956076+DoVanThang118@users.noreply.github.com>
2023-05-11 20:51:43 +0700

    new

--- src/App.js
+++ src/App.js
@@ -17,9 +17,16 @@ class App extends React.Component{
           tel: ["0923232"]
         }
       ],
-      className: "T2203E"
+      className: "T2203E",
+      new_student:{
+        name: "",
+        mark: 0,
+        tel: ""
+      }
     }
     this.changeClassName = this.changeClassName.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.submitStudent = this.submitStudent.bind(this);
   }
 
   changeClassName(event){
@@ -29,11 +36,32 @@ class App extends React.Component{
     })
   }
 
+  handleInput(event){
+    const input = event.target;
+    const new_student = this.state.new_student;
+    new_student[input.name] = input.value;
+    this.setState({
+      new_student: new_student
+    })
+  }
+
+  submitStudent(event){
+    event.preventDefault();
+    const new_student = this.state.new_student;
+    new_student.tel = [new_student.tel];
+    const students = this.state.students;
+    students.push(new_student);
+    this.setState({students:students});
+  }
+
   render(){
     const students = this.state.students;
     const className = this.state.className;
+    const new_student = this.state.new_student;
+
     return (
       <div className="App">
+
         <h1> Student List: {className}</h1>
         {
           students.map((v,k)=>{
@@ -42,8 +70,17 @@ class App extends React.Component{
         }
         <hr/>
         <input onChange={this.changeClassName} type='text' value={className} placeholder= 'enter class name ...' />
+
+        <h1>Add Student</h1>
+        <form method="post" onSubmit={this.submitStudent}>
+          <input onChange={this.handleInput} value={new_student.name} name="name" type="text"  placeholder= 'enter name ...'/>
+          <input onChange={this.handleInput} value={new_student.mark} name="mark" type="number"  placeholder= 'enter tel ...'/>
+          <input onChange={this.handleInput} value={new_student.tel} name="tel" type="text"  placeholder= 'enter tel ...'/>
+          <button type="submit">Submit</button>
+        </form>
+
       </div>
-  );
+    );
   }
 }
 
