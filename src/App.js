c5e7f85cab0ae5c94d000bc3642040fb2fa32263
DoVanThang118 <102956076+DoVanThang118@users.noreply.github.com>
2023-05-13 19:00:54 +0700

    new

--- src/App.js
+++ src/App.js
@@ -5,79 +5,52 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      students:[
-        {
-          name: "Student 01",
-          mark: 8,
-          tel: ["0912345"]
-        },
-        {
-          name: "Student 00",
-          mark: 12,
-          tel: ["0923232"]
-        }
-      ],
-      className: "T2203E",
-      new_student:{
-        name: "",
-        mark: 0,
-        tel: ""
-      }
+      products: []
     }
-    this.changeClassName = this.changeClassName.bind(this);
-    this.handleInput = this.handleInput.bind(this);
-    this.submitStudent = this.submitStudent.bind(this);
   }
-
-  changeClassName(event){
-    const v = event.target.value;
-    this.setState({
-      className: v
+  componentDidMount(){
+    const url = "https://dummyjson.com/products?limit=20";
+    fetch(url)
+    .then(rs=>rs.json())
+    .then(rs=>{
+      this.setState({
+        products:rs.products
+      })
     })
-  }
-
-  handleInput(event){
-    const input = event.target;
-    const new_student = this.state.new_student;
-    new_student[input.name] = input.value;
-    this.setState({
-      new_student: new_student
+    .catch(err=>{
+      alert(err);
     })
-  }
-
-  submitStudent(event){
-    event.preventDefault();
-    const new_student = this.state.new_student;
-    new_student.tel = [new_student.tel];
-    const students = this.state.students;
-    students.push(new_student);
-    this.setState({students:students});
   }
 
   render(){
-    const students = this.state.students;
-    const className = this.state.className;
-    const new_student = this.state.new_student;
-
+    const products = this.state.products;
     return (
-      <div className="App">
-        <h1> Student List: {className}</h1>
-        {
-          students.map((v,k)=>{
-            return <Student key={k} name={v.name} mark={v.mark} tel={v.tel}/>
-          })
-        }
-        <hr/>
-        <input onChange={this.changeClassName} type='text' value={className} placeholder= 'enter class name ...' />
-
-        <h1>Add Student</h1>
-        <form method="post" onSubmit={this.submitStudent}>
-          <input onChange={this.handleInput} value={new_student.name} name="name" type="text"  placeholder= 'enter name ...'/>
-          <input onChange={this.handleInput} value={new_student.mark} name="mark" type="number"  placeholder= 'enter tel ...'/>
-          <input onChange={this.handleInput} value={new_student.tel} name="tel" type="text"  placeholder= 'enter tel ...'/>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
+      <section>
+        <div className="container">
+          <h1>Danh sachs san pham</h1>
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            {
+              products.map((e,k)=>{
+                return (
+                  <div key={k} className = "col">
+                    <div className="card h-100">
+                      <div className="card">
+                        <img src={e.thumbnail} className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                          <h5 className="card-title">{e.title}</h5>
+                          <p className="card-text">{e.description}</p>
+                          <p className="text-danger">{e.price}</p>
+                          <a href="#" className="btn btn-primary">Buy</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      </section>
     );
   }
 }
